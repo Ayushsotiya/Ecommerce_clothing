3c281dcbec041d5b6797fc48b3dd3c9a3ec3242e
Ayush Sotiya <ayushsotiya56@gmail.com>
2026-04-22 19:21:11 +0530

    fixing imports locations

--- server/agent/index.js
+++ server/agent/index.js
@@ -6,13 +6,13 @@
  * 1. Conversational Agent (Chat)
  * 2. Product Analysis Agent (Vision)
  */
-
 // Export Chat Agent
 const { processChat } = require('./chat');
 const { rateLimiter, getRemainingRequests, RATE_LIMIT_CONFIG } = require('./shared/rateLimiter');
 
 // Export Product Analysis Agent
 const { analyzeProductImages } = require('./product-analysis');
+
 
 module.exports = {
     // Chat Agent
@@ -24,4 +24,5 @@ module.exports = {
     // Product Analysis Agent
     analyzeProductImages
 };
+
 
